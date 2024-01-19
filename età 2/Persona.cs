46de6829cd33c620d26c7b7a3050140f3680e362
lara.passanti@gmail.com <lara.passanti@gmail.com>
2024-01-19 19:11:30 +0100

    Aggiungere i file di progetto.

---
+++ età 2/Persona.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace età_2
+{
+    internal class Persona
+    {
+        public DateTime datadinascita { get; set; }
+
+
+        public int Età()
+        {
+            DateTime oggi = DateTime.Today;
+            if(oggi.Month < datadinascita.Month) { return oggi.Year - datadinascita.Year -1 ;  }
+            if(oggi.Month > datadinascita.Month) { return oggi.Year - datadinascita.Year ; }
+            else 
+            {
+                if(oggi.Day > datadinascita.Day || oggi.Day==datadinascita.Day) { return oggi.Year - datadinascita.Year; }
+                else { return oggi.Year - datadinascita.Year - 1; }
+            }
+            
+        }
+    }
+}
---
+++ età 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace età_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { Persona p = new Persona();
+            Console.WriteLine("inserire la data di nascita");
+            p.datadinascita=DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("l'età è: {0}", p.Età()); 
+
+            Console.ReadLine();
+        }
+    }
+}
